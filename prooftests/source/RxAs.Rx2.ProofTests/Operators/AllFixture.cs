05329e9a582142438f20e145c39a1de6016dcb93
richardszalay <richard@richardszalay.com>
2010-06-09 17:39:14 +0100

    Added a proof test for all (empty source)

---
+++ prooftests/source/RxAs.Rx2.ProofTests/Operators/AllFixture.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using RxAs.Rx2.ProofTests.Mock;
+
+namespace RxAs.Rx2.ProofTests.Operators
+{
+    [TestFixture]
+    public class AllFixture
+    {
+        [Test]
+        public void empty_sequence()
+        {
+            var stats = new StatsObserver<bool>();
+
+            Observable.Empty<int>()
+                .All(x => true)
+                .Subscribe(stats);
+
+            Assert.IsTrue(stats.NextValues[0]);
+
+        }
+    }
+}
